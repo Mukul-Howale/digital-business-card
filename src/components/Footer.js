9598657ddcc629ab4cd7bf6f1fcf549543e1c471
Mukul Howale <howalemukul@gmail.com>
2022-09-28 02:59:41 +0530

    Added footer component and changed stylings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
+import twitter_icon from "../images/twitter_icon.png";
+import facebook_icon from "../images/facebook-icon.png";
+import instagram_icon from "../images/instagram-icon.png";
+import linkedin_icon from "../images/linkedin-icon.png";
+import github_icon from "../images/gitHub-icon.png";
+import "../styles/Footer.css";
 
 export default function Footer(){
     return (
-        <h1>Footer goes here</h1>
+        <div className="footer">
+            <img src={twitter_icon} alt="Twitter Icon"></img>
+            <img src={facebook_icon} alt="Facebook Icon"></img>
+            <img src={instagram_icon} alt="Instagram Icon"></img>
+            <img src={linkedin_icon} alt="LinkedIn Icon"></img>
+            <img src={github_icon} alt="Github Icon"></img>
+        </div>
     )
 }
