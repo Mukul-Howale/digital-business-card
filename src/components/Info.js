8d5bbdfd467c755da83f975b61e3bf3047e4e5f3
Mukul Howale <howalemukul@gmail.com>
2022-09-27 23:46:59 +0530

    created info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,19 @@
 import React from "react";
+import my_image from "../images/my_image.jpg";
+import mail_icon from "../images/mail_icon.png";
+import "../styles/Info.css";
 
 export default function Info() {
     return (
-        <h1>Info goes here!</h1>
+        <div className="info">
+            <img src={my_image} alt="This is me" className="my-photo"></img>
+            <p className="name">Mukul Howale</p>
+            <p className="person-title">Full Stack Developer</p>
+            <a href="https://mukulhowale.netlify.app" className="portfolio-link">My Portfolio</a>
+            <div className="email-button">
+                <img src={mail_icon} alt="Email Icon"></img>
+                <p>Email</p>
+            </div>
+        </div>
     )
 }
