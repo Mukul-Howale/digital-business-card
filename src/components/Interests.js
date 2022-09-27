c681c8a27ffa44d018f075122346b6d16cab5343
Mukul Howale <110479646+Mukul-Howale@users.noreply.github.com>
2022-09-28 02:31:46 +0530

    Created interest section (#3)

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "../styles/App.css";
 
 export default function Interests(){
     return (
-        <h1>Interests goes here</h1>
+        <div className="section">
+            <p className="section-title">Interests</p>
+            <p className="section-details">
+            Music scholar. Reader. Internet fanatic. Bacon buff. 
+            Entrepreneur. Gamming Geek.
+            </p>
+        </div>
     )
 }
